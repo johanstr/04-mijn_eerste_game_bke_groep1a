5558b12ff7d5b874b35f3cb19a20e5cb549d2dec
Johan Strootman <johanstr@users.noreply.github.com>
2025-01-09 16:53:06 +0100

    Resultaat na 09-01-2025
    Globale variabelen aangemaakt.

--- js/bke.js
+++ js/bke.js
@@ -4,3 +4,12 @@
  *  Versie:     2025
  **************************************************/
 
+// ELEMENTS
+let element_turn_number;
+let element_turn_image;
+let element_score_player1;
+let element_score_player2;
+let element_round_time;
+let element_rounds_played;
+let element_cells;
+let element_game_button;
